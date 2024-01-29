dff767a7c55dcc63300caac999c0d0e159d21c56
abidhussain09 <abidhussain0495@gmail.com>
2024-01-29 12:14:57 +0530

    sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import { useState } from 'react'
+
 import './App.css'
+import SideBar from './components/Sidebar'
 
 function App() {
 
   return (
     <>
-      <div>
-        
+      <div className='flex'>
+        <div className='max-w-[240px]'>
+        <SideBar/>
+        </div>
+        <div>
+
+        </div>
       </div>
     </>
   )
