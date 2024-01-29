4f78d388a1e20500e1d85a1f21865c7ce300c609
student-nitp <gvaishnavi069@gmail.com>
2024-01-29 12:27:24 +0530

    resolved merge conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,10 @@
 
 import './App.css'
-<<<<<<< HEAD
-import ComplaintForm from './components/ComplaintForm'
-=======
-import SideBar from './components/Sidebar'
->>>>>>> refs/remotes/origin/main
 
 function App() {
 
   return (
     <>
-<<<<<<< HEAD
-      <div>
-        <ComplaintForm/>
-=======
-      <div className='flex'>
-        <div className='max-w-[240px]'>
-        <SideBar/>
-        </div>
-        <div>
-
-        </div>
->>>>>>> refs/remotes/origin/main
-      </div>
     </>
   )
 }
